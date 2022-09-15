351ba8ec086d99b9507364110abfe211c89ba157
SnehalBali <100759130+SnehalBali@users.noreply.github.com>
2022-09-16 00:56:00 +0530

    Delete QuickPharma directory

--- QuickPharma/src/main/java/com/example/repository/ManufactureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.entity.Manufacture;
-
-public interface ManufactureRepository extends CrudRepository<Manufacture, Integer> {
-
-}
--- QuickPharma/src/main/java/com/example/repository/ProductDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.entity.ProductDetails;
-
-public interface ProductDetailsRepository extends CrudRepository<ProductDetails, Integer>{
-
-}
